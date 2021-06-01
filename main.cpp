faedb956885f8c9c0ed08b35faf6f6dd3ea2f5b9
darian31 <dsnovikova_1@miem.hse.ru>
2021-06-01 21:27:30 +0300

    Загрузка новеллы

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include "tree.cpp"
+#include <fstream>
+#include <string>
+int main() {
+    std::vector<page> pageTree;
+    std::ifstream fin;
+    fin.open("tree");
+    char line[2048];
 
-int main(){
-    std::cout << "Hello World!" << '\n';
+    while(!fin.eof()){
+        page pageNode;
+        fin.getline(line, 2048);
+        fin.getline(line, 2048);
+        std::string name;
+        std::string text;
+
+        fin.getline(line, 2048);
+        name = line;
+        fin.getline(line, 2048);
+        text = line;
+        fin.getline(line, 2048);
+
+        std::string line1;
+        line1 = line;
+
+        while(line1 != ")"){
+            pageNode.link.push_back(atoi(line1.c_str()));
+            fin.getline(line, 2048);
+            line1 = line;
+        }
+        pageNode.name = name;
+        pageNode.text = text;
+        pageTree.push_back(pageNode);
+    }
     
+
     return 0;
 }
---
+++ tree.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <map>
+#include <vector>
+
+struct page{
+    std::string name;
+    std::string text;
+
+    std::vector<int> link;
+
+};
